2cc4255661543b0c3f46ef906e2c3513b1850eb1
mohankumar27 <mohankumar27197@gmail.com>
2020-08-05 15:47:25 +0530

    EarthQuake Report App (First commit)

--- app/src/main/java/com/example/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/quakereport/QueryUtils.java
@@ -14,7 +14,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
